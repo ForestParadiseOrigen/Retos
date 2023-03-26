af381d57de0ef480de2ec2d5c0f06eb889849165
Andres Cordoba <andres.jcv.678@gmail.com>
2023-03-26 15:41:50 -0500

    Agregamos el ultimo reto de matrizes.

--- src/reto11.java
+++ src/reto11.java
@@ -1,3 +1,29 @@
+import java.util.Scanner;
+
 public class reto11 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+
+         //Declaramos la matriz
+        String [][] pro = new String [4][4]; //Producto y precio
+
+        //Pedimos los datos qu van a ir a las matrizes
+        for (int c = 0; c < 4; c++) { 
+            for (int f = 0; f < 4; f++) { 
+                System.out.println("Digite el nombre y  precio del producto en la culimna: " + (c+1) + " y en la fila " + (f+1) + "."); 
+                pro[c][f] = sc.next();//Producto y precio           
+            }
+            System.out.println();
+        } 
+
+        //Imprimir los datos de la matriz
+        for (int c = 0; c < 4; c++) { 
+            for (int f = 0; f < 4; f++) { 
+                System.out.print(pro[c][f] +"\t"); //imprime producto y precio                 
+            } 
+            System.out.println(); 
+
+        } 
+        sc.close();
+    }
 }
