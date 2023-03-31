b65d00eefa8d6d4d7a6654bbaf9ef2c8cbfdbf4c
Andres Cordoba <andres.jcv.678@gmail.com>
2023-03-30 19:33:50 -0500

    Correccion reto 7

--- src/reto7.java
+++ src/reto7.java
@@ -8,32 +8,32 @@ public class reto7 {
 
         do{
             //Recoleccion de datos.
-        System.out.println("<--FORMULARIO-->");
-        System.out.println("Diligencie el siguiente formulario.");
+            System.out.println("<--FORMULARIO-->");
+            System.out.println("Diligencie el siguiente formulario.");
 
-        System.out.println("Nombre:"); nombre = sc.next();
-        System.out.println("Apellido: "); apellido = sc.next();
-        System.out.println("Ciudad de nacimiento: "); ciudad = sc.next();
-        System.out.println("Edad: "); edad = sc.nextInt();
-        System.out.println("FECHA DE NACIMIENTO");
-        System.out.println("Dia: "); dia = sc.nextInt();
-        System.out.println("Numero del mes: "); mes = sc.nextInt();
-        System.out.println("Año: "); año = sc.nextInt();
-        System.out.println("Numero de identificacion: "); id = sc.nextInt();
+            System.out.println("Nombre:"); nombre = sc.next();
+            System.out.println("Apellido: "); apellido = sc.next();
+            System.out.println("Ciudad de nacimiento: "); ciudad = sc.next();
+            System.out.println("Edad: "); edad = sc.nextInt();
+            System.out.println("FECHA DE NACIMIENTO");
+            System.out.println("Dia: "); dia = sc.nextInt();
+            System.out.println("Numero del mes: "); mes = sc.nextInt();
+            System.out.println("Año: "); año = sc.nextInt();
+            System.out.println("Numero de identificacion: "); id = sc.nextInt();
 
-        System.out.println("--------------------------------------------------------------------");
+            System.out.println("--------------------------------------------------------------------");
 
-        //Impresion.
-        System.out.println("El registro ha quedado de la siguienye forma: ");
-        System.out.println("Nombre: " + nombre + ".");
-        System.out.println("Apellido: " + apellido + ".");
-        System.out.println("Ciudad de nacimiento: " + ciudad + ".");
-        System.out.println("Edad: " + edad + ".");
-        System.out.println("Fecha de nacimiento: " + dia + "/" + mes + "/" + año + ".");
-        System.out.println("Numero de identificacion: " + id + ".");
+            //Impresion.
+            System.out.println("El registro ha quedado de la siguienye forma: ");
+            System.out.println("Nombre: " + nombre + ".");
+            System.out.println("Apellido: " + apellido + ".");
+            System.out.println("Ciudad de nacimiento: " + ciudad + ".");
+            System.out.println("Edad: " + edad + ".");
+            System.out.println("Fecha de nacimiento: " + dia + "/" + mes + "/" + año + ".");
+            System.out.println("Numero de identificacion: " + id + ".");
 
-        System.out.println("¿Desea volver a llenar el formulario?\nResponda:\nSi: s\nNo: n");
-        r = sc.next();
+            System.out.println("¿Desea volver a llenar el formulario?\nResponda:\nSi: s\nNo: n");
+            r = sc.next();
         }while(r.equalsIgnoreCase("s"));
 
         sc.close();
